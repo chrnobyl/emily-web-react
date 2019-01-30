bb5bff1ebebd265734ee46f5eb206cd395540af1
github username <cjab86@gmail.com>
2019-01-30 15:36:27 -0500

    added all logos to logo grid

--- src/Logos.js
+++ src/Logos.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Image } from 'semantic-ui-react'
 // import { Link, Switch, Route } from 'react-router-dom'
 import './App.css';
 
@@ -7,6 +8,29 @@ const Logos = (props) => {
     <div className='logo-grid'>
       <div className='logo-intro'>
         I've written for these companies:
+      </div>
+      <div className='logo-description'>
+        Here is where the description goes
+      </div>
+      <div className='redfin'>
+      </div>
+      <div className='mtv'>
+      </div>
+      <div className='jerusalem-post'>
+      </div>
+      <div className='grey'>
+      </div>
+      <div className='xfinity'>
+      </div>
+      <div className='portersail'>
+      </div>
+      <div className='fabfitfun'>
+      </div>
+      <div className='secret-escapes'>
+      </div>
+      <div className='people'>
+      </div>
+      <div className='buzzfeed'>
       </div>
     </div>
   )
