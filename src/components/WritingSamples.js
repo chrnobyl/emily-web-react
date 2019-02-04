626d3d68ec5af787c7bd8b78f015cba58c71ed20
github username <cjab86@gmail.com>
2019-02-04 17:51:38 -0500

    sized carousel to app pics

--- src/components/WritingSamples.js
+++ src/components/WritingSamples.js
@@ -3,7 +3,7 @@ import { Transition, Segment, Header } from 'semantic-ui-react'
 import { Link, Switch, Route } from 'react-router-dom'
 import ImageCarousel from './ImageCarousel'
 import 'pure-react-carousel/dist/react-carousel.es.css'
-import '../App.css';
+import '../App.css'
 
 export default class WritingSamples extends Component {
   state = { visible: false }
@@ -18,24 +18,16 @@ export default class WritingSamples extends Component {
     const { visible } = this.state
 
     return (
-      <Route path="/writing_samples" render={() =>
+      <Route path='/writing_samples' render={() =>
         <Transition visible={visible} animation='fade' duration={800}>
-          <div className='block'>
-            <h1 style={{fontFamily: 'Comfortaa'}}>Here are some writing samples</h1>
-
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus efficitur at nibh nec placerat. In pharetra est nec quam cursus, nec malesuada turpis pulvinar. Aliquam mattis, ipsum quis fringilla ornare, orci ante accumsan neque, vel tempus mauris urna vitae ipsum. Suspendisse et fringilla velit, eu aliquet nibh. Suspendisse luctus turpis vitae nisi aliquet, quis dictum sem molestie. Sed eu enim ut urna finibus convallis in at enim. Aliquam eu nunc porttitor, pretium purus quis, congue lorem. Donec elementum ut risus a venenatis. Sed molestie facilisis nulla, quis fermentum dolor tempor et. Integer quis scelerisque leo, vitae luctus enim. Phasellus euismod suscipit fringilla. Suspendisse sed porttitor quam.
-            </p>
-            <Segment attached="top">
-              <Header as="h2" content="Image carousel" />
+          <div className='samples-grid'>
+            <div className='samples-text'>
+              <h1 style={{fontFamily: 'Comfortaa'}}>Here are some writing samples</h1>
               <p>
-                This prototype features how to create a carousel with images, take a
-                look into <code>examples/ImageCarousel</code> to get more details.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus efficitur at nibh nec placerat. In pharetra est nec quam cursus, nec malesuada turpis pulvinar. Aliquam mattis, ipsum quis fringilla ornare, orci ante accumsan neque, vel tempus mauris urna vitae ipsum. Suspendisse et fringilla velit, eu aliquet nibh. Suspendisse luctus turpis vitae nisi aliquet, quis dictum sem molestie. Sed eu enim ut urna finibus convallis in at enim. Aliquam eu nunc porttitor, pretium purus quis, congue lorem. Donec elementum ut risus a venenatis. Sed molestie facilisis nulla, quis fermentum dolor tempor et. Integer quis scelerisque leo, vitae luctus enim. Phasellus euismod suscipit fringilla. Suspendisse sed porttitor quam.
               </p>
-            </Segment>
-            <Segment attached="bottom">
-              <ImageCarousel />
-            </Segment>
+            </div>
+            <ImageCarousel className='carousel'/>
           </div>
         </Transition>
       }/>
