22e649669167226ee397b039fece82f3df6df33b
github username <cjab86@gmail.com>
2019-02-06 18:08:33 -0500

    added styling to home css module

--- src/components/Container/Home/Home.js
+++ src/components/Container/Home/Home.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Link, Switch, Route } from 'react-router-dom'
+import styles from './Home.module.css'
 import { Parallax, Background } from 'react-parallax'
 import { Grid, Image, Transition } from 'semantic-ui-react'
 import TravelGuides from './TravelGuides/TravelGuides'
 import Lifestyle from './Lifestyle/Lifestyle'
 import Logos from './Logos/Logos'
 import WorkTogether from './WorkTogether/WorkTogether'
-import '../../../App.css'
+// import '../../../App.css'
 
 export default class Home extends Component {
   state = { visible: false }
@@ -21,20 +22,20 @@ export default class Home extends Component {
     const { visible } = this.state
 
     return (
-      <Route path="/" render={() =>
-        <div className='home-grid'>
-          <div className='intro'>
+      <Route path='/' render={() =>
+        <div className={styles['home-grid']}>
+          <div className={styles['intro']}>
               I've written for several publications on travel, lifestyle, and real estate. My true passion lies in discovering new places and finding the best of what they have to offer.
           </div>
-          <div className='travel-pic'>
+          <div className={styles['travel-pic']}>
           </div>
           <TravelGuides />
-          <div className='lifestyle-pic'>
+          <div className={styles['lifestyle-pic']}>
           </div>
           <Lifestyle />
           <Logos />
           <WorkTogether />
-          <div className='work-pic'>
+          <div className={styles['work-pic']}>
           </div>
         </div>
         }/>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -17,28 +17,28 @@ export default class MenuExampleInvertedSecondary extends Component {
           position='right'
           active={activeItem === 'home'}
           onClick={this.handleItemClick}>
-          <h3 className="nav">Home</h3>
+          <h3 className='nav'>Home</h3>
         </Menu.Item>
         <Menu.Item name='about'
           as={Link} to='/about'
           position='right'
           active={activeItem === 'about'}
           onClick={this.handleItemClick}>
-          <h3 className="nav">About</h3>
+          <h3 className='nav'>About</h3>
         </Menu.Item>
         <Menu.Item
           name='samples'
           as={Link} to='/writing_samples'
           active={activeItem === 'samples'}
           onClick={this.handleItemClick}>
-          <h3 className="nav">Writing Samples</h3>
+          <h3 className='nav'>Writing Samples</h3>
         </Menu.Item>
         <Menu.Item
           name='contact'
           as={Link} to='/contact'
           active={activeItem === 'contact'}
           onClick={this.handleItemClick}>
-          <h3 className="nav">Contact</h3>
+          <h3 className='nav'>Contact</h3>
         </Menu.Item>
       </Menu>
     )
