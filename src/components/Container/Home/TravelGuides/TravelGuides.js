fb733b93922a96519b5ba6f820acff59bc2e81a4
github username <cjab86@gmail.com>
2019-02-07 11:13:10 -0500

    added css modules for Logos, WorkTogether, Lifestyle

--- src/components/Container/Home/TravelGuides/TravelGuides.js
+++ src/components/Container/Home/TravelGuides/TravelGuides.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
-import '../../../../App.css';
+import styles from './TravelGuides.module.css'
+
 
 const TravelGuides = (props) => {
   return (
-    <div className='travel-guides'>
+    <div className={styles['travel-guides']}>
       <p>
         Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus. Etiam gravida tellus consectetur tellus feugiat, sit amet pretium ligula suscipit.
       </p>
