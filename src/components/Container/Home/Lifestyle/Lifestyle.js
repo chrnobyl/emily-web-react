b321f2a62ada3797c6e285f7f130ac2a874cc009
github username <cjab86@gmail.com>
2019-02-07 11:15:43 -0500

    added lifestyle css module

--- src/components/Container/Home/Lifestyle/Lifestyle.js
+++ src/components/Container/Home/Lifestyle/Lifestyle.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
-import '../../../../App.css';
+import styles from './Lifestyle.module.css'
 
 const Lifestyle = (props) => {
   return (
-    <div className='lifestyle'>
+    <div className={styles['lifestyle']}>
       <p>
         Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus. Etiam gravida tellus consectetur tellus feugiat, sit amet pretium ligula suscipit.
       </p>
