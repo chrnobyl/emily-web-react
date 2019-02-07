bc4a21cfb5783a94f80fcb7e095078b918db1efb
github username <cjab86@gmail.com>
2019-02-07 11:01:15 -0500

    finalized writing sample image location, added css module for nav bar

--- src/components/Container/WritingSamples/ImageCarousel/ImageCarousel.js
+++ src/components/Container/WritingSamples/ImageCarousel/ImageCarousel.js
@@ -1,15 +1,17 @@
 import { CarouselProvider, Image, Slide, Slider } from 'pure-react-carousel'
 import React from 'react'
+import styles from './ImageCarousel.module.css'
 import { Divider } from 'semantic-ui-react'
-import CustomDotGroup from './CustomDotGroup/CustomDotGroup'
+import CustomDotGroup from '../CustomDotGroup/CustomDotGroup'
 
 const ImageCarousel = (props) => (
-  <CarouselProvider naturalSlideWidth={1} naturalSlideHeight={2} totalSlides={props.images.length}>
+  <CarouselProvider className={styles['carousel']} naturalSlideWidth={1} naturalSlideHeight={2} totalSlides={props.images.length}>
+  {console.log(props.location)}
     <Slider>
       {
         props.images.map((name, index) => (
         <Slide tag='a' index={index} key={index}>
-          <Image src={require(`${props.location}/${name + props.extension}`)} />
+          <Image src={require(`../${props.location}/${name + props.extension}`)} />
         </Slide>
         )
       )}
