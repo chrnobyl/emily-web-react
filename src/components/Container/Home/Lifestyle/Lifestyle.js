5e168f551e2571bfb8b49fd1b0f667c3e168e44c
github username <cjab86@gmail.com>
2019-02-07 14:42:12 -0500

    added transition for Lifestyle

--- src/components/Container/Home/Lifestyle/Lifestyle.js
+++ src/components/Container/Home/Lifestyle/Lifestyle.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import { Button } from 'semantic-ui-react'
+import { Button, Transition } from 'semantic-ui-react'
 import styles from './Lifestyle.module.css'
 
-const Lifestyle = (props) => {
-  return (
-    <div className={styles['lifestyle']}>
-      <p>
-        Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus. Etiam gravida tellus consectetur tellus feugiat, sit amet pretium ligula suscipit.
-      </p>
-      <Button as={Link} to='/writing_samples'>Lifestyle Pieces</Button>
-    </div>
-  )
-}
+export default class Lifestyle extends Component {
+  state = { visible: false }
 
-export default Lifestyle
+  toggleVisibility = () => this.setState({ visible: !this.state.visible })
+
+  componentDidMount() {
+    this.toggleVisibility()
+  }
+
+  render() {
+    const { visible } = this.state
+
+    return (
+      <Transition visible={visible} animation='fade' duration={800}>
+        <div className={styles['lifestyle']}>
+          <p>
+            Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus. Etiam gravida tellus consectetur tellus feugiat, sit amet pretium ligula suscipit.
+          </p>
+          <Button as={Link} to='/writing_samples'>Lifestyle Pieces</Button>
+        </div>
+      </Transition>
+    )
+  }
+}
