8b32e58e8c399513e1f4313686959b1df5c2ff96
github username <cjab86@gmail.com>
2019-02-08 17:40:38 -0500

    added buzzfeed samples

--- src/components/Container/WritingSamples/Lifestyle/Buzzfeed/Buzzfeed.js
+++ src/components/Container/WritingSamples/Lifestyle/Buzzfeed/Buzzfeed.js
@@ -6,7 +6,16 @@ const Buzzfeed = (props) => {
   return (
     <List size='medium' relaxed>
       <List.Item>
-        <Sample title={'What It Feels Like To Be Single In New York During The Spring'} images={['buzzfeed_single_in_ny']} extension={'.pdf'}/>
+        <a target='_blank' href='https://www.buzzfeed.com/efh45/17-thoughts-all-casual-skiiers-know-to-be-true-8b8t'>17 Thoughts All Casual Skiiers Know To Be True</a>
+      </List.Item>
+      <List.Item>
+        <a target='_blank' href='https://www.buzzfeed.com/efh45/jewelry-retailer-blocks-customer-for-instagramming-8b8t'>Jewelry Retailer Blocks Customer For Instagramming About Missing Christmas Package</a>
+      </List.Item>
+      <List.Item>
+        <a target='_blank' href='https://www.buzzfeed.com/efh45/is-germanys-world-cup-coach-actually-friday-night-8b8t'>Is That Germany's World Cup Coach Or Is It Friday Night Lights' Coach Taylor?</a>
+      </List.Item>
+      <List.Item>
+        <a target='_blank' href='https://www.buzzfeed.com/efh45/what-its-like-to-be-single-in-new-york-in-the-spr-8b8t'>What It Feels Like To Be Single In New York During The Spring</a>
       </List.Item>
     </List>
   )
