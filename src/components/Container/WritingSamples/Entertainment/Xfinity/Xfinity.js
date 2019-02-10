f472e217e71febf68498c850d6ae9f1876130b08
github username <cjab86@gmail.com>
2019-02-10 18:53:18 -0500

    added xfinity to entertainment page

---
+++ src/components/Container/WritingSamples/Entertainment/Xfinity/Xfinity.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { List, Modal, Header } from 'semantic-ui-react'
+import Sample from '../../Sample/Sample'
+
+const Xfinity = (props) => {
+  return (
+    <List size='medium' relaxed>
+      <List.Item>
+        <a target='_blank' href='https://es.xfinity.com/sdmy/blogs/tv/2009/06/03/rescue-me-star-denis-leary-answers-burning-questions/'>‘Rescue Me’ Star Denis Leary Answers Burning Questions</a>
+      </List.Item>
+    </List>
+  )
+}
+
+export default Xfinity
