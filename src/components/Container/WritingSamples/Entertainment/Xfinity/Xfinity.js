c433a48827be500e0ad47ca07c941bf5834933de
github username <cjab86@gmail.com>
2019-02-11 16:39:11 -0500

    fixed links to articles

--- src/components/Container/WritingSamples/Entertainment/Xfinity/Xfinity.js
+++ src/components/Container/WritingSamples/Entertainment/Xfinity/Xfinity.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { List } from 'semantic-ui-react'
 
 const Xfinity = (props) => {
   return (
     <List size='medium' relaxed>
       <List.Item>
-        <a target='_blank' href='https://es.xfinity.com/sdmy/blogs/tv/2009/06/03/rescue-me-star-denis-leary-answers-burning-questions/'>‘Rescue Me’ Star Denis Leary Answers Burning Questions</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://es.xfinity.com/sdmy/blogs/tv/2009/06/03/rescue-me-star-denis-leary-answers-burning-questions/'>‘Rescue Me’ Star Denis Leary Answers Burning Questions</a>
       </List.Item>
       <List.Item>
-        <a target='_blank' href='https://es.xfinity.com/sdmy/blogs/tv/2009/08/20/project-runways-jeffrey-sebelia-where-is-he-now/'>Project Runway’s Jeffrey Sebelia: Where is He Now?</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://es.xfinity.com/sdmy/blogs/tv/2009/08/20/project-runways-jeffrey-sebelia-where-is-he-now/'>Project Runway’s Jeffrey Sebelia: Where is He Now?</a>
       </List.Item>
       <List.Item>
-        <a target='_blank' href='https://es.xfinity.com/sdmy/blogs/tv/2009/07/23/bravo-stars-nikki-cascone-and-christian-siriano-team-up/'>Bravo Stars Nikki Cascone and Christian Siriano Team Up</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://es.xfinity.com/sdmy/blogs/tv/2009/07/23/bravo-stars-nikki-cascone-and-christian-siriano-team-up/'>Bravo Stars Nikki Cascone and Christian Siriano Team Up</a>
       </List.Item>
       <List.Item>
-        <a target='_blank' href='https://es.xfinity.com/sdmy/blogs/tv/author/emilyhochberg/page/1/'>See more...</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://es.xfinity.com/sdmy/blogs/tv/author/emilyhochberg/page/1/'>See more...</a>
       </List.Item>
     </List>
   )
--- src/components/Container/WritingSamples/Lifestyle/JerusalemPost/JerusalemPost.js
+++ src/components/Container/WritingSamples/Lifestyle/JerusalemPost/JerusalemPost.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { List } from 'semantic-ui-react'
 
 const JerusalemPost = (props) => {
   return (
     <List size='medium' relaxed>
       <List.Item>
-        <a target='_blank' href='https://www.jpost.com/Travel/Around-Israel/Riding-high'>Riding High</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://www.jpost.com/Travel/Around-Israel/Riding-high'>Riding High</a>
       </List.Item>
       <List.Item>
-        <a target='_blank' href='https://www.jpost.com/Local-Israel/Tel-Aviv-And-Center/No-smoking-166424'>No Smoking...!</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://www.jpost.com/Local-Israel/Tel-Aviv-And-Center/No-smoking-166424'>No Smoking...!</a>
       </List.Item>
       <List.Item>
-        <a target='_blank' href='https://www.jpost.com/Local-Israel/Recycling-Hanukka'>Recycling Hanukka</a>
+        <a target='_blank' rel='noopener noreferrer' href='https://www.jpost.com/Local-Israel/Recycling-Hanukka'>Recycling Hanukka</a>
       </List.Item>
     </List>
   )
