8a524ef169aa3e7d343ac7d8c3c11e2f747a9cdc
github username <cjab86@gmail.com>
2019-02-11 11:47:23 -0500

    added jp writing samples

--- src/components/Container/WritingSamples/Lifestyle/JerusalemPost/JerusalemPost.js
+++ src/components/Container/WritingSamples/Lifestyle/JerusalemPost/JerusalemPost.js
@@ -6,13 +6,13 @@ const JerusalemPost = (props) => {
   return (
     <List size='medium' relaxed>
       <List.Item>
-        <Sample title={'Riding High'} images={['jp_riding_high']} extension={'.pdf'}/>
+        <a target='_blank' href='https://www.jpost.com/Travel/Around-Israel/Riding-high'>Riding High</a>
       </List.Item>
       <List.Item>
-        <Sample title={"Tel Aviv's New Home Away From Home"} images={['jp_tel_aviv_home_away']} extension={'.pdf'}/>
+        <a target='_blank' href='https://www.jpost.com/Local-Israel/Tel-Aviv-And-Center/No-smoking-166424'>No Smoking...!</a>
       </List.Item>
       <List.Item>
-        <Sample title={'No Smoking...!'} images={['jp_no_smoking']} extension={'.pdf'}/>
+        <a target='_blank' href='https://www.jpost.com/Local-Israel/Recycling-Hanukka'>Recycling Hanukka</a>
       </List.Item>
     </List>
   )
