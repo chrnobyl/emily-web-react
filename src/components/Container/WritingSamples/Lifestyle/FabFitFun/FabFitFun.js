8cce262879605e524c5ff93623838c509cfaadcc
github username <cjab86@gmail.com>
2019-02-10 19:41:33 -0500

    added fff links, removed fff and xfinity images

--- src/components/Container/WritingSamples/Lifestyle/FabFitFun/FabFitFun.js
+++ src/components/Container/WritingSamples/Lifestyle/FabFitFun/FabFitFun.js
@@ -7,13 +7,25 @@ const FabFitFun = (props) => {
   return (
     <List size='medium' relaxed>
       <List.Item>
-        <Sample title={'15 Secret Starbucks Frappes You Should Order On National Coffee Day'} images={Constants.fabFitFun} extension={'.jpg'}/>
+        <a target='_blank' href='https://fabfitfun.com/magazine/learn-new-language-with-these-apps/'>Learning a New Language? Download These 5 Apps</a>
       </List.Item>
       <List.Item>
-        <Sample title={"Tel Aviv's New Home Away From Home"} images={['jp_tel_aviv_home_away']} extension={'.pdf'}/>
+        <a target='_blank' href='https://fabfitfun.com/magazine/products-need-throw-last-minute-pool-party/'>The Only Products You Need to Throw a Last-Minute Pool Party</a>
       </List.Item>
       <List.Item>
-        <Sample title={'No Smoking...!'} images={['jp_no_smoking']} extension={'.pdf'}/>
+        <a target='_blank' href='https://fabfitfun.com/magazine/national-coffee-day/'>15 Secret Starbucks Frappes You Should Order on National Coffee Day</a>
+      </List.Item>
+      <List.Item>
+        <a target='_blank' href='https://fabfitfun.com/magazine/recipes-for-avocado-obsessed/'>10 Must-Make Recipes for Anyone Avocado-Obsessed</a>
+      </List.Item>
+      <List.Item>
+        <a target='_blank' href='https://fabfitfun.com/magazine/jean-washing-guide/'>How Often Do You Really Need to Wash Your Jeans?</a>
+      </List.Item>
+      <List.Item>
+        <a target='_blank' href='https://fabfitfun.com/magazine/dear-hollywood-please-dont-ever-remake-80s-movies/'>Dear Hollywood, Please Don’t Ever Remake These ’80s Movies</a>
+      </List.Item>
+      <List.Item>
+        <a target='_blank' href='https://fabfitfun.com/magazine/80s-items-will-make-super-nostalgic/'>Nine ‘80s Items That Will Make You Super Nostalgic</a>
       </List.Item>
     </List>
   )
