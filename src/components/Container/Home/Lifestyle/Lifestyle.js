c9b8cdbab94ccb4f3f31950ae1cf81e89e2bcd16
github username <cjab86@gmail.com>
2019-02-12 16:33:36 -0500

    routed lifestyle button to lifestyle page

--- src/components/Container/Home/Lifestyle/Lifestyle.js
+++ src/components/Container/Home/Lifestyle/Lifestyle.js
@@ -21,7 +21,7 @@ export default class Lifestyle extends Component {
           <p>
             Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus. Etiam gravida tellus consectetur tellus feugiat, sit amet pretium ligula suscipit.
           </p>
-          <Button as={Link} to='/writing_samples'>Lifestyle Pieces</Button>
+          <Button as={Link} to='/lifestyle'>Lifestyle Pieces</Button>
         </div>
       </Transition>
     )
