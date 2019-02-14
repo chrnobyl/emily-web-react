fe4b9c39d628b12ceeb79e7f7498fcab0930c8a3
github username <cjab86@gmail.com>
2019-02-14 16:36:07 -0500

    configured about divs

--- src/components/Container/About/About.js
+++ src/components/Container/About/About.js
@@ -18,11 +18,11 @@ export default class About extends Component {
     return (
       <Route path="/about" render={() =>
         <Transition visible={visible} animation='fade' duration={800}>
-          <div className={styles['container']}>
+          <div className={styles['about-container']}>
 
-            <div className={styles['item1']}>
+            <div className={styles['about-pic']}>
             </div>
-            <div className={styles['item2']}>
+            <div className={styles['about-text']}>
               <h1 style={{fontFamily: 'Comfortaa'}}>About info goes here</h1>
               <p>
                 Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus. Etiam gravida tellus consectetur tellus feugiat, sit amet pretium ligula suscipit.
