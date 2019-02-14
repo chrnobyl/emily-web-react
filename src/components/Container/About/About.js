a49985f8da2c5da913cc0ecf9ae0ddab7dea2da6
github username <cjab86@gmail.com>
2019-02-14 15:38:44 -0500

    added picture and bio to about

--- src/components/Container/About/About.js
+++ src/components/Container/About/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-import { Transition } from 'semantic-ui-react'
+import { Transition, Image } from 'semantic-ui-react'
 import styles from './About.module.css'
 
 export default class About extends Component {
@@ -18,11 +18,11 @@ export default class About extends Component {
     return (
       <Route path="/about" render={() =>
         <Transition visible={visible} animation='fade' duration={800}>
-          <div className={styles['about-grid']}>
+          <div className={styles['container']}>
 
-            <div className={styles['about-pic']}>
+            <div className={styles['item1']}>
             </div>
-            <div className={styles['about-text']}>
+            <div className={styles['item2']}>
               <h1 style={{fontFamily: 'Comfortaa'}}>About info goes here</h1>
               <p>
                 Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus. Etiam gravida tellus consectetur tellus feugiat, sit amet pretium ligula suscipit.
