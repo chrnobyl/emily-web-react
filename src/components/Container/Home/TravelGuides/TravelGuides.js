9f2d7ab046e57e71cd656c92f9ceb627b242f962
github username <cjab86@gmail.com>
2019-02-24 22:57:19 -0500

    shortened lifestyle and travel text

--- src/components/Container/Home/TravelGuides/TravelGuides.js
+++ src/components/Container/Home/TravelGuides/TravelGuides.js
@@ -19,7 +19,7 @@ export default class TravelGuides extends Component {
       <Transition visible={visible} animation='fade' duration={800}>
         <div className={styles['travel-guides']}>
           <p>
-            Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus. Etiam gravida tellus consectetur tellus feugiat, sit amet pretium ligula suscipit.
+            Pellentesque pulvinar id orci eu accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean pellentesque convallis nulla, ac mattis nisl malesuada et. Donec ut nulla nibh. Quisque eget mi lorem. Aliquam tortor odio, pulvinar eget pretium sit amet, semper nec lorem. Vestibulum volutpat tempor cursus.
           </p>
           <Button as={Link} to='/travel'>Travel Guides</Button>
         </div>
